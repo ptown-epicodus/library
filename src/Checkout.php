cf7502c234134100371e58f7ff7ed5b88f8c96a0
Patrick McGreevy and Zach Swanson <zach@>
2017-03-02 10:32:38 -0800

    add checkout class with save, getall, deleteall

---
+++ src/Checkout.php
@@ -0,0 +1,97 @@
+<?php
+    class Checkout
+    {
+        private $id;
+        private $copy_id;
+        private $patron_id;
+        private $checkout_date;
+        private $return_date;
+        private $due_date;
+
+        function __construct($copy_id, $patron_id, $checkout_date, $due_date, $return_date = "0000-00-00", $id = null)
+        {
+            $this->copy_id = $copy_id;
+            $this->patron_id = $patron_id;
+            $this->checkout_date = $checkout_date;
+            $this->due_date = $due_date;
+            $this->return_date = $return_date;
+            $this->id = $id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getCopyId()
+        {
+            return $this->copy_id;
+        }
+        function setCopyId($new_id)
+        {
+            $this->copy_id = $new_id;
+        }
+
+        function getPatronId()
+        {
+            return $this->patron_id;
+        }
+        function setPatronId($new_id)
+        {
+            $this->patron_id = $new_id;
+        }
+
+        function getCheckoutDate()
+        {
+            return $this->checkout_date;
+        }
+        function setCheckoutDate($new_date)
+        {
+            $this->checkout_date = $new_date;
+        }
+
+        function getReturnDate()
+        {
+            return $this->return_date;
+        }
+        function setReturnDate($new_date)
+        {
+            $this->return_date = $new_date;
+        }
+
+        function getDueDate()
+        {
+            return $this->due_date;
+        }
+        function setDueDate($new_date)
+        {
+            $this->due_date = $new_date;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO checkouts (copy_id, patron_id, checkout_date, return_date, due_date) VALUES ({$this->getCopyId()}, {$this->getPatronId()}, '{$this->getCheckoutDate()}', '{$this->getReturnDate()}', '{$this->getDueDate()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $checkouts = [];
+            $query = $GLOBALS['DB']->query("SELECT * FROM checkouts;");
+            // $checkouts = $query->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Checkout", ["copy_id", "patron_id", "checkout_date", "due_date", "return_date", "id"]);
+            foreach ($query as $checkout)
+            {
+                $new_checkout = new Checkout($checkout['copy_id'], $checkout['patron_id'], $checkout['checkout_date'], $checkout['due_date'], $checkout['return_date'], $checkout['id']);
+                array_push($checkouts, $new_checkout);
+            }
+            return $checkouts;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM checkouts;");
+        }
+
+
+    }
+?>
---
+++ tests/CheckoutTest.php
@@ -0,0 +1,95 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once 'src/Checkout.php';
+    require_once 'src/Book.php';
+    require_once 'src/Copy.php';
+    require_once 'src/Patron.php';
+
+    $server = 'mysql:host=localhost:8889;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class CheckoutTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Checkout::deleteAll();
+            Book::deleteAll();
+            Patron::deleteAll();
+            Copy::deleteAll();
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $title = 'Bible';
+            $test_Book = new Book($title);
+            $test_Book->save();
+            $book_id = $test_Book->getId();
+
+            $test_copy = new Copy($book_id);
+            $test_copy->save();
+
+            $name = 'Jim';
+            $test_patron = new Patron($name);
+            $test_patron->save();
+
+            $copy_id= $test_copy->getId();
+            $patron_id= $test_patron->getId();
+            $checkout_date = "2017-02-02";
+            $due_date = "2017-02-02";
+            $test_Checkout = new Checkout($copy_id, $patron_id, $checkout_date, $due_date);
+
+            // Act
+            $test_Checkout->save();
+            $result = Checkout::getAll();
+
+            // Assert
+            $this->assertEquals([$test_Checkout],$result);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $title = 'Bible';
+            $test_Book = new Book($title);
+            $test_Book->save();
+            $book_id = $test_Book->getId();
+
+            $test_copy = new Copy($book_id);
+            $test_copy->save();
+
+            $name = 'Jim';
+            $test_patron = new Patron($name);
+            $test_patron->save();
+
+            $copy_id= $test_copy->getId();
+            $patron_id= $test_patron->getId();
+            $checkout_date = "2017-02-02";
+            $due_date = "2017-02-02";
+            $test_Checkout = new Checkout($copy_id, $patron_id, $checkout_date, $due_date);
+            $test_Checkout->save();
+
+            $checkout_date2 = "2017-03-03";
+            $due_date2 = "2017-03-14";
+            $test_Checkout2 = new Checkout($copy_id, $patron_id, $checkout_date2, $due_date2);
+            $test_Checkout2->save();
+
+            // Act
+            $result = Checkout::getAll();
+
+            // Assert
+            $this->assertEquals([$test_Checkout, $test_Checkout2],$result);
+        }
+
+
+
+
+
+    }
+?>
